81e3308af7f3e212f0f846f4d6d6ef00326925c6
Caleb Wolthers <w0473807@nscc.ca>
2024-01-09 10:07:06 -0400

    Messing around with structs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,50 @@ namespace Hello_Structs_CalebWolthers
 {
     internal class Program
     {
+
+        struct Person
+        {
+
+            public string name;
+
+            public int age;
+        
+        }
+
+
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Structs");
+            Console.WriteLine("");
+
+            Person person1 = new Person();
+
+            person1.name = "john";
+            person1.age = 28;
+
+
+            Person person2 = person1;
+
+            person2.name = "Drac";
+
+            Console.WriteLine("Person Information");
+            Console.WriteLine(person1.name);
+            Console.WriteLine(person1.age);
+            Console.WriteLine("");
+
+            Console.WriteLine("Person Information");
+            Console.WriteLine(person2.name);
+            Console.WriteLine(person2.age);
+            Console.WriteLine("");
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+
         }
     }
 }
